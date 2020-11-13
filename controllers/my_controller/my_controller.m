8c4de55df9c0b67dd68700d22c3fe2bc55f8ddc0
Quercusstorm <74053004+Quercusstorm@users.noreply.github.com>
2020-11-14 00:31:06 +0100

    front_move_robot

--- controllers/my_controller/my_controller.m
+++ controllers/my_controller/my_controller.m
@@ -15,7 +15,13 @@ TIME_STEP = 64;
 % get and enable devices, e.g.:
 %  camera = wb_robot_get_device('camera');
 %  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
+motor_left = wb_robot_get_device('motor_left');
+motor_right = wb_robot_get_device('motor_right');
+
+wb_motor_set_position(motor_left, inf);
+wb_motor_set_velocity(motor_left, 1);
+wb_motor_set_position(motor_right, inf);
+wb_motor_set_velocity(motor_right, 1);
 
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
